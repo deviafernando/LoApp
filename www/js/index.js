b15b6feec7093f8e425dc3f75b7f423984f4a254
deviafernando <deviafernando@hotmail.com>
2015-06-30 15:11:03 -0500

    Last version temporal

--- www/js/index.js
+++ www/js/index.js
@@ -9,113 +9,6 @@ var breadcrumbsNavigation = [];
 var menuStatus=false;
 var readyToExit=false;
 var contentLoadingTimeOut;
-var pushNotification;
-
-function setUpNotifications() {
-
-	try
-	{
-		pushNotification = window.plugins.pushNotification;
-
-		if (device.platform == 'android' || device.platform == 'Android' || device.platform == 'amazon-fireos' ) {
-			pushNotification.register(successHandler, errorHandler, {"senderID":"583978535652","ecb":"onNotification"});
-		} else {
-			pushNotification.register(tokenHandler, errorHandler, {"badge":"true","sound":"true","alert":"true","ecb":"onNotificationAPN"});
-		}
-	}
-	catch(err)
-	{
-		txt="There was an error on this page.\n\n";
-		txt+="Error description: " + err.message + "\n\n";
-		setVisibleText(txt);
-	}
-}
-
-// handle APNS notifications for iOS
-function onNotificationAPN(e) {
-	if (e.alert) {
-		navigator.notification.alert(e.alert);
-	}
-
-	if (e.sound) {
-		var snd = new Media(e.sound);
-		snd.play();
-	}
-
-	if (e.badge) {
-		pushNotification.setApplicationIconBadgeNumber(successHandler, e.badge);
-	}
-}
-
-// handle GCM notifications for Android
-function onNotification(e) {
-	setVisibleText('<li>EVENT -> RECEIVED:' + e.event + '</li>');
-
-	switch( e.event )
-	{
-		case 'registered':
-			if ( e.regid.length > 0 )
-			{
-				setVisibleText('<li>REGISTERED -> REGID:' + e.regid + "</li>");
-				console.log("regID = " + e.regid);
-				//TODO e.regid to send and save
-			}
-			break;
-
-		case 'message':
-			if (e.foreground)
-			{
-				setVisibleText('<li>--INLINE NOTIFICATION--' + '</li>');
-				var soundfile = e.soundname || e.payload.sound;
-				var my_media = new Media("/android_asset/www/"+ soundfile);
-
-				my_media.play();
-			}
-			else
-			{
-				if (e.coldstart)
-					setVisibleText('<li>--COLDSTART NOTIFICATION--' + '</li>');
-				else
-					setVisibleText('<li>--BACKGROUND NOTIFICATION--' + '</li>');
-			}
-
-			setVisibleText('<li>MESSAGE -> MSG: ' + e.payload.message + '</li>');
-			//android only
-			setVisibleText('<li>MESSAGE -> MSGCNT: ' + e.payload.msgcnt + '</li>');
-			//amazon-fireos only
-			setVisibleText('<li>MESSAGE -> TIMESTAMP: ' + e.payload.timeStamp + '</li>');
-			break;
-
-		case 'error':
-			setVisibleText('<li>ERROR -> MSG:' + e.msg + '</li>');
-			break;
-
-		default:
-			setVisibleText('<li>EVENT -> Unknown, an event was received and we do not know what it is</li>');
-			break;
-	}
-}
-
-function tokenHandler (result) {
-	setVisibleText('<li>token: '+ result +'</li>');
-	// Your iOS push server needs to know the token before it can push to this device
-	// here is where you might want to send it the token for later use.
-}
-
-function successHandler (result) {
-	setVisibleText('<li>success:'+ result +'</li>');
-}
-
-function errorHandler (error) {
-	setVisibleText('<li>error:'+ error +'</li>');
-}
-
-
-
-
-
-
-
 
 function isOnInternet(){
 	if(navigator.connection.type=="Connection.NONE"){
@@ -128,6 +21,7 @@ function isOnInternet(){
 }
 
 function setVisibleText(message){
+	//$("#debuger").append(message);
 	return true;
 }
 
@@ -749,3 +643,139 @@ document.addEventListener('deviceready', onDeviceReady, false);
 return true;
 
 });
+
+//NOTIFICATIONS
+var pushNotification;
+
+function setNotificationVisibleText(message){
+	//$("#debuger").append(message);
+}
+
+function setUpNotifications() {
+	setNotificationVisibleText('<li>deviceready event received</li>');
+
+	try
+	{
+		pushNotification = window.plugins.pushNotification;
+		setNotificationVisibleText('<li>registering ' + device.platform + '</li>');
+		if (device.platform == 'android' || device.platform == 'Android' || device.platform == 'amazon-fireos' ) {
+			pushNotification.register(successHandler, errorHandler, {"senderID":"183128038002","ecb":"onNotification"});
+		} else {
+			pushNotification.register(tokenHandler, errorHandler, {"badge":"true","sound":"true","alert":"true","ecb":"onNotificationAPN"});	// required!
+		}
+	}
+	catch(err)
+	{
+		txt="There was an error on this page.\n\n";
+		txt+="Error description: " + err.message + "\n\n";
+		setNotificationVisibleText(txt);
+	}
+}
+
+// handle APNS notifications for iOS
+function onNotificationAPN(e) {
+	if (e.alert) {
+		setNotificationVisibleText('<li>push-notification: ' + e.alert + '</li>');
+		// showing an alert also requires the org.apache.cordova.dialogs plugin
+		navigator.notification.alert(e.alert);
+	}
+
+	if (e.sound) {
+		// playing a sound also requires the org.apache.cordova.media plugin
+		var snd = new Media(e.sound);
+		snd.play();
+	}
+
+	if (e.badge) {
+		pushNotification.setApplicationIconBadgeNumber(successHandler, e.badge);
+	}
+}
+
+function sendRegId(regid){
+	$.ajax({
+		url: 'http://m.enibague.com/saveregid.php',
+		type: 'GET',
+		data: 'regid='+regid,
+		success: function(data) {
+			setNotificationVisibleText(data);
+		},
+		error: function(e) {
+
+		}
+	});
+}
+
+
+// handle GCM notifications for Android
+function onNotification(e) {
+	setNotificationVisibleText('<li>EVENT -> RECEIVED:' + e.event + '</li>');
+
+	switch( e.event )
+	{
+		case 'registered':
+			if ( e.regid.length > 0 )
+			{
+				setNotificationVisibleText('<li>REGISTERED -> REGID:' + e.regid + "</li>");
+				// Your GCM push server needs to know the regID before it can push to this device
+				// here is where you might want to send it the regID for later use.
+				setNotificationVisibleText("regID = " + e.regid);
+				sendRegId(e.regid);
+			}
+			break;
+
+		case 'message':
+			// if this flag is set, this notification happened while we were in the foreground.
+			// you might want to play a sound to get the user's attention, throw up a dialog, etc.
+			if (e.foreground)
+			{
+				setNotificationVisibleText('<li>--INLINE NOTIFICATION--' + '</li>');
+
+				// on Android soundname is outside the payload.
+				// On Amazon FireOS all custom attributes are contained within payload
+				var soundfile = e.soundname || e.payload.sound;
+				// if the notification contains a soundname, play it.
+				// playing a sound also requires the org.apache.cordova.media plugin
+				var my_media = new Media("/android_asset/www/"+ soundfile);
+
+				my_media.play();
+			}
+			else
+			{	// otherwise we were launched because the user touched a notification in the notification tray.
+				if (e.coldstart)
+					setNotificationVisibleText('<li>--COLDSTART NOTIFICATION--' + '</li>');
+				else
+					setNotificationVisibleText('<li>--BACKGROUND NOTIFICATION--' + '</li>');
+			}
+
+			setNotificationVisibleText('<li>MESSAGE -> MSG: ' + e.payload.message + '</li>');
+			//android only
+			setNotificationVisibleText('<li>MESSAGE -> MSGCNT: ' + e.payload.msgcnt + '</li>');
+			//amazon-fireos only
+			setNotificationVisibleText('<li>MESSAGE -> TIMESTAMP: ' + e.payload.timeStamp + '</li>');
+			break;
+
+		case 'error':
+			setNotificationVisibleText('<li>ERROR -> MSG:' + e.msg + '</li>');
+			break;
+
+		default:
+			setNotificationVisibleText('<li>EVENT -> Unknown, an event was received and we do not know what it is</li>');
+			break;
+	}
+}
+
+function tokenHandler (result) {
+	setNotificationVisibleText('<li>token: '+ result +'</li>');
+	// Your iOS push server needs to know the token before it can push to this device
+	// here is where you might want to send it the token for later use.
+}
+
+function successHandler (result) {
+	setNotificationVisibleText('<li>success:'+ result +'</li>');
+}
+
+function errorHandler (error) {
+	setNotificationVisibleText('<li>error:'+ error +'</li>');
+}
+
+
